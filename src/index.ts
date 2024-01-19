0787edfa2803c4c9b145ce6fb84abf1c4e50083a
IgnacioCava <cavaignacio11@gmail.com>
2024-01-18 22:38:53 -0300

    fix: change server startup to fix cors

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,35 @@
 // The ApolloServer constructor requires two parameters: your schema
 // npm install @apollo/server graphql
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
+import http from 'http'
 import { typeDefs, resolvers } from './schema/index.js'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { expressMiddleware } from '@apollo/server/express4'
+import cors from 'cors'
+import express from 'express'
+import * as dotenv from 'dotenv'
 
-interface MyContext {
-	token?: string
-}
+dotenv.config()
 
-const server = new ApolloServer<MyContext>({
+const app = express()
+const httpServer = http.createServer(app)
+
+const server = new ApolloServer({
 	typeDefs,
 	resolvers,
-	introspection: true
+	introspection: true,
+	cache: 'bounded',
+	plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 })
 
-const { url } = await startStandaloneServer(server, {
-	context: async ({ req }) => ({ token: req.headers.token }),
-	listen: { port: 4000 }
-})
+await server.start()
 
-console.log(`🚀 Server ready at ${url}`)
+const corsOptions: cors.CorsOptions = {
+	origin: [process.env.CLIENT_ORIGIN || 'http://localhost:3000', 'https://studio.apollographql.com'],
+	credentials: true
+}
+
+app.use('/', cors<cors.CorsRequest>(corsOptions), express.json(), expressMiddleware(server))
+
+await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve))
+console.log(`🚀 Server ready at http://localhost:4000/`)
