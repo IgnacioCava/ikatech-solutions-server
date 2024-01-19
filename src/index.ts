ea983082698ea38dd507f6047d1d0d629f72b76e
IgnacioCava <cavaignacio11@gmail.com>
2024-01-19 11:05:12 -0300

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,13 @@ const httpServer = http.createServer(app)
 const server = new ApolloServer<MyContext>({
 	typeDefs,
 	resolvers,
+	introspection: true,
 	plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 })
 await server.start()
+
+app.use(cors())
+app.use(express.json())
 
 app.use(
 	'/graphql',
@@ -26,19 +30,17 @@ app.use(
 	express.json(),
 	expressMiddleware(server, {
 		context: async ({ req }) => ({ token: req.headers.token })
-	})
-)
-
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*')
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-	if (req.method == 'OPTIONS') {
-		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-		return res.status(200).json({})
+	}),
+	(req, res, next) => {
+		res.header('Access-Control-Allow-Origin', '*')
+		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+		if (req.method == 'OPTIONS') {
+			res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+			return res.status(200).json({})
+		}
+		next()
 	}
-
-	next()
-})
+)
 
 await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve))
 console.log('ready')
